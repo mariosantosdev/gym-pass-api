40da3e11942a99df0384177009d251aab85a295d
Mário Santos <mariodev7@gmail.com>
2023-06-23 13:06:02 -0300

    feat(errors): create a handle to unknown error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
 import fastify from 'fastify'
 
 import { Routes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(Routes)
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(422)
+      .send({ message: 'Validation Errors', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') console.error(error)
+
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
--- src/http/controllers/Users/RegisterUserController.ts
+++ src/http/controllers/Users/RegisterUserController.ts
@@ -25,7 +25,7 @@ export async function RegisterUserController(
       return reply.status(409).send()
     }
 
-    return reply.status(500).send()
+    throw error
   }
 
   return reply.status(201).send()
