a8d4f21ec8c044ce0cd621147ff09a16be8136c0
Mário Santos <mariodev7@gmail.com>
2023-06-25 16:27:30 -0300

    feat(http-controller): create route to sign in

---
+++ src/http/controllers/Auth/SignInController.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { PrismaUsersRepository } from '~src/repositores/prisma/prisma-users-repository'
+import { InvalidCredentialsError } from '~src/errors/InvalidCredentialsError'
+import { SignInService } from '~src/services/Auth/SignInService'
+
+export async function SignInController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const signInBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+  })
+
+  const { email, password } = signInBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const signInUseCase = new SignInService(usersRepository)
+    await signInUseCase.execute({ email, password })
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send()
+    }
+
+    throw error
+  }
+
+  return reply.status(200).send()
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
 import { RegisterUserController } from './controllers/Users/RegisterUserController'
+import { SignInController } from './controllers/Auth/SignInController'
 
 export async function Routes(app: FastifyInstance) {
   app.post('/users', RegisterUserController)
+  app.post('/sessions', SignInController)
 }
