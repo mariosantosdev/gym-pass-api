b37fc767afa768e775e5c240d7b042f771898835
Mário Santos <mariodev7@gmail.com>
2023-07-06 20:26:01 -0300

    feat(gyms): create gym repository

--- src/repositores/in-memory/in-memory-gyms-repository.ts
+++ src/repositores/in-memory/in-memory-gyms-repository.ts
@@ -1,5 +1,6 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 import { GymsRepository } from '../gyms-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
@@ -8,6 +9,23 @@ export class InMemoryGymsRepository implements GymsRepository {
     const gym = this.items.find((gym) => gym.id === gymId)
 
     if (!gym) return null
+
+    return gym
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      name: data.name,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+
+    this.items.push(gym)
 
     return gym
   }
