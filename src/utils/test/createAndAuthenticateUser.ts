4adf7cb036060f594c2b0524dfe1ec9ad6fbe8ea
Mário Santos <mariodev7@gmail.com>
2023-07-23 14:50:52 -0300

    refactor(create-and-authenticate-use):  receive role as optional

--- src/utils/test/createAndAuthenticateUser.ts
+++ src/utils/test/createAndAuthenticateUser.ts
@@ -1,21 +1,27 @@
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
+import { prisma } from '~src/lib/prisma'
+import { hash } from 'bcryptjs'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  const email = 'johndoe@example.com'
-
-  await request(app.server).post('/users').send({
-    name: 'John Doe',
-    email,
-    password: '12345678',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  role: 'ADMIN' | 'MEMBER' = 'MEMBER',
+) {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: await hash('12345678', 8),
+      role,
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
-    email,
+    email: user.email,
     password: '12345678',
   })
 
   const { token } = authResponse.body
 
-  return { token, email }
+  return { token, email: user.email }
 }
