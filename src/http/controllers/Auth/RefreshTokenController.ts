59569ed8797344886a7fc3c9dfd4543c4531dbb7
Mário Santos <mariodev7@gmail.com>
2023-07-23 14:29:37 -0300

    feat(refresh-token-controller): create http route to refresh token

---
+++ src/http/controllers/Auth/RefreshTokenController.ts
@@ -0,0 +1,38 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function RefreshTokenController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  console.log('ola')
+  await request.jwtVerify({ onlyCookie: true })
+
+  const token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+      },
+    },
+  )
+
+  const refreshToken = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+        expiresIn: '7d',
+      },
+    },
+  )
+
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({ token })
+}
--- src/http/controllers/Auth/routes.ts
+++ src/http/controllers/Auth/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
 import { SignInController } from './SignInController'
+import { RefreshTokenController } from './RefreshTokenController'
 
 export async function authRoutes(app: FastifyInstance) {
   app.post('/sessions', SignInController)
+  app.patch('/token/refresh', RefreshTokenController)
 }
